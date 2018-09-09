0229dc9e5da09e8805610e67949f712594ce3b48
FlyingDance1995 <39576535+FlyingDance1995@users.noreply.github.com>
2018-09-09 19:46:53 +0700

    yeo  man

---
+++ Yeo-Man/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeoManApp
+ * @description
+ * # yeoManApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeoManApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ Yeo-Man/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeoManApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yeoManApp
+ */
+angular.module('yeoManApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
